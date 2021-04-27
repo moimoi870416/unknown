b6825b1d1461bd35e32220800c7b860e5c854db6
moimoi870416 <moimoi870416@gmail.com>
2021-04-27 20:18:07 +0800

    no message

--- src/sence/menuScene/EnterScene.java
+++ src/sence/menuScene/EnterScene.java
@@ -60,7 +60,9 @@ public class EnterScene extends Scene {
         this.start = new Button(1100, 720, Theme.get(9));
         start.setClickedActionPerformed((x, y) -> {
             if (isServer && !change) {
-                ConnectController.getInstance().changeSceneSend(isNormal);
+//                ConnectController.getInstance().changeSceneSend(isNormal);
+                ConnectController.getInstance().changeBossSceneSend();
+
                 change = true;
             }
         });
